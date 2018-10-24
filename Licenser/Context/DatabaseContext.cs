988fa5e6c9e82fcd0683bb29c4d41d3091a67771
NandaBhattarai <nandabhattarai1@gmail.com>
2018-10-24 15:50:39 +0545

    first push

---
+++ Licenser/Context/DatabaseContext.cs
@@ -0,0 +1,21 @@
+﻿using Licenser.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace Licenser.Context
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext() : base("LicenserDatabase")
+        { }
+        public DbSet<Student> Students { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ Licenser/Migrations/201810240900206_first.cs
@@ -0,0 +1,29 @@
+namespace Licenser.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class first : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Student",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        DoB = c.DateTime(nullable: false),
+                        Gender = c.Int(nullable: false),
+                        Flag = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Student");
+        }
+    }
+}
