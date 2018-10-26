2e485ba569edf258a276fcc4e812b795bd6388f6
NandaBhattarai <nandabhattarai1@gmail.com>
2018-10-26 17:11:08 +0545

    product model

--- Licenser/Controllers/ProductController.cs
+++ Licenser/Controllers/ProductController.cs
@@ -18,7 +18,7 @@ namespace Licenser.Controllers
             return View(db.Products.ToList());
         }
         public ActionResult Create()
-        {           
+        {
             return View();
         }
         [HttpPost]
@@ -27,9 +27,9 @@ namespace Licenser.Controllers
             if (ModelState.IsValid)
             {
                 db.Products.Add(model);
-                if(db.SaveChanges() > 0)
+                if (db.SaveChanges() > 0)
                 {
-                    return RedirectToAction("Index","Product");
+                    return RedirectToAction("Index", "Product");
                 }
                 else
                 {
@@ -55,6 +55,46 @@ namespace Licenser.Controllers
                 return HttpNotFound();
             }
             return View(pro);
+        }
+        [HttpPost]
+        public JsonResult Edit(Product prod)
+        {
+            Product pro = db.Products.Find(prod.ProductId);
+            if (ModelState.IsValid)
+            {
+              
+                pro.ProductId = prod.ProductId;
+                pro.Name = prod.Name;
+                pro.Description = prod.Description;
+                pro.HomePageUrl = prod.HomePageUrl;
+                pro.DownloadUrl = prod.DownloadUrl;
+                pro.Version = prod.Version;
+                pro.Status = prod.Status;
+                if (db.SaveChanges() > 0)
+                {
+                   
+                    return Json(new { result = true, message="Successfully Changed" }, JsonRequestBehavior.AllowGet);
+                }
+                else
+                {
+                    return Json(new { result = false, message = "No Changes" }, JsonRequestBehavior.AllowGet);
+                }
+            }
+            return Json(new { message="Validation Error" }, JsonRequestBehavior.AllowGet);
+        }
+        [HttpPost]
+        public ActionResult Delete(int ProductId)
+        {
+            Product pro = db.Products.Find(ProductId);
+            db.Products.Remove(pro);
+            if (db.SaveChanges() > 0)
+            {
+                return Json(new { result = true, message = "Successfully Deleted" }, JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                return Json(new { result = false, message = "No Changes" }, JsonRequestBehavior.AllowGet);
+            }            
         }
     }
 }
